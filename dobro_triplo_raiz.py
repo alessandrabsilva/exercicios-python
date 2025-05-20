aa5704dd405213b437e39df4db3b92bca4b9ec6e
alessandrabsilva <alessandrabernardinosilva@gmail.com>
2025-05-20 10:18:48 -0300

    Create dobro_triplo_raiz.py
    Create dobro_triplo_raiz.py

---
+++ dobro_triplo_raiz.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+dobro = n * 2
+triplo = n * 3
+raiz = n ** (1/2)
+print('O seu dobro é {},\n o seu tripo é {}\n e sua raiz quadrada é {:.2f}'.format(dobro,triplo, raiz))
